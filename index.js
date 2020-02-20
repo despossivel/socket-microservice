3a0655480589018cf420d78e4ddc1b06ee3793c9
matheus <matheus@ti>
2020-02-20 17:42:00 -0300

    socket

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const server = app.listen(process.env.PORT || 5005, () => console.log('SERVER ON-LINE'));
 const io = require('socket.io').listen(server);
 
-io.origins(['*']).on('connection', async (socket) => {
+io.origins('*:*').on('connection', async (socket) => {
     const application = app;
 
     socket.on('registrar', async (dados) => {
@@ -18,7 +18,7 @@ io.origins(['*']).on('connection', async (socket) => {
     socket.on('transferencia', async (dados) => {
         const { _idDestinatario } = dados;
         console.log(dados)
-        io.to(_idDestinatario).emit('receiveDebit', dados)
+        io.to(_idDestinatario).emit('receive', dados)
     })
 
     socket.on('disconnect', () => console.log('Usuario desconectado!'));
