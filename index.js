4fee13c7e7435ae5d6c9ec384fe871a431b5b554
matheus <matheus@ti>
2020-02-20 16:08:17 -0300

    cors

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app = express();
 const server = app.listen(process.env.PORT || 5005, () => console.log('SERVER ON-LINE'));
 const io = require('socket.io').listen(server);
 
-io.origins('*:*').on('connection', async (socket) => {
+io.origins('*').on('connection', async (socket) => {
     const application = app;
 
     socket.on('registrar', async (dados) => {
         const { _id } = dados;
+        console.log(dados)
         socket.join(_id);
         socket.broadcast.to(_id).emit('event');
     });
@@ -16,6 +17,7 @@ io.origins('*:*').on('connection', async (socket) => {
     //socket para debitar pontos e enviar atualização para o destinatario
     socket.on('transferencia', async (dados) => {
         const { _idDestinatario } = dados;
+        console.log(dados)
         io.to(_idDestinatario).emit('receiveDebit', dados)
     })
 
